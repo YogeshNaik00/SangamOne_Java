540cf7277726ca0cdd27b1eb259e1a7d5ca56a0d
YogeshNaik00 <yogeshnaik.sangamone@gmail.com>
2021-09-20 15:07:34 +0530

    First commit

---
+++ src/main/java/com/sangamone/test/SangamOneJavaApplication.java
@@ -0,0 +1,13 @@
+package com.sangamone.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SangamOneJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SangamOneJavaApplication.class, args);
+	}
+
+}
